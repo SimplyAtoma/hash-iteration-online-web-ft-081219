cbe597957e36c11268b3e7d2ed08796dd3ba76c9
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-18 20:10:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each{ |name|
+  birthday_kids.each{ ||
       puts "Happy Birthday #{birthday_kids[name]}! You are now #{birthday_kids[name]} years old!\n"}
 end
 
